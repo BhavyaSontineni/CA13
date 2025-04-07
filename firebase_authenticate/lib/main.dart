2cd39e3dbc5a2b008ffc26c9c8d0db73826686c6
BhavyaSontineni <bsontineni1@student.gsu.edu>
2025-04-07 16:59:37 -0400

    Add ProfileScreen with user email display, change password functionality, and logout feature

--- firebase_authenticate/lib/main.dart
+++ firebase_authenticate/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
+//sontineni Bhavya Sri Sai-002893685
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
